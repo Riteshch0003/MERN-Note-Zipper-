d734ccbf53164aac29b8a823e60f09a8c4404976
ritesh chopra <riteshch0003@gmail.com>
2024-12-20 12:01:38 +0530

    changes

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -3,12 +3,13 @@ const notFound=(req,res,next)=>{
     res.status(404);
     next(error);
 };
-const errorHanler=(err,res,req,next)=>{
-    const statusCode=res.statusCode===200?500:res.statusCode;
-    res.status(statusCode);
-    res.json({
-        message:err.message,
-        stack:process.env.NODE_ENV==="production"?null:err.stack,
-    })
+// errorMiddleware.js
+
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500;
+    res.status(statusCode).json({
+        message: err.message,
+    });
 };
-module.exports= {notFound,errorHanler};
+
+module.exports = { notFound, errorHandler };
--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,32 @@
 const express=require("express");
-const notes = require("./data/notes");
 // set the env in different file to make secret
 const dotenv=require('dotenv');
 const connectDb=require('./Config/Db');
 const userRoutes=require('./routes/userRoutes');
-const { notFound, errorHanler } = require("./middleware/errorMiddleware");
+const noteRoutes=require('./routes/noteRoutes')
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const app =express();
 dotenv.config();
 connectDb();
 app.use(express.json())
 
 
-app.get('/',(req,res)=>{
-    res.send("API is running")
-})
-app.get('/api/notes',(req,res)=>{
-    res.json(notes)
-})
+// app.get('/',(req,res)=>{
+//     res.send("API is running")
+// })
+// app.get('/api/notes',(req,res)=>{
+//     res.json(notes)
+// })
 // the uid from th eapi to get the single data by parameters
-app.get('/api/notes/:id',(req,res)=>{
-const note =notes.find((n)=>n._id===req.params.id)
-res.send(note)
-})
+// app.get('/api/notes/:id',(req,res)=>{
+// const note =notes.find((n)=>n._id===req.params.id)
+// res.send(note)
+// })
+
 app.use('/api/users',userRoutes);
+app.use('/api/notes',noteRoutes);
 app.use(notFound);
-app.use(errorHanler)
+app.use(errorHandler);
 const PORT=process.env.PORT||5000;
 // Express is like telling your app to start listening for visitors on a specific address and port
 app.listen(PORT,console.log(`server started on PORT ${PORT}`)
